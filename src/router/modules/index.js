49d118811729faca752733b3b1550dd1e3a9172d
jiang.wang <jiang.wang@opay-inc.com>
2022-03-22 18:50:13 +0800

    123
    Signed-off-by: jiang.wang <jiang.wang@opay-inc.com>

--- src/router/modules/index.js
+++ src/router/modules/index.js
@@ -1,10 +1,10 @@
 export default [
   {
-    path: "/",
-    component: () => import("@/views/index.vue"),
+    path: '/',
+    component: () => import('@/views/index.vue')
   },
   {
-    path: "/page",
-    component: () => import("@/views/page.vue"),
-  },
+    path: '/page',
+    component: () => import('@/views/page.vue')
+  }
 ]
