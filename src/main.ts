656c560ff2aab3aad8fe8b301c2157f2d2ff7225
jiang.wang <jiang.wang@opay-inc.com>
2022-03-22 14:36:09 +0800

    format
    Signed-off-by: jiang.wang <jiang.wang@opay-inc.com>

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import { createApp } from "vue";
-import router, { setupRouter } from "@/router/index.js";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import router, { setupRouter } from '@/router/index.js'
+import App from './App.vue'
 
 import 'virtual:windi.css'
 import 'virtual:windi-devtools'
 
-const app = createApp(App);
+const app = createApp(App)
 
-setupRouter(app);
-await router.isReady();
-app.mount("#app");
+setupRouter(app)
+await router.isReady()
+app.mount('#app')
