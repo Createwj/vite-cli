55ac7bd46c5129c8031fba956ea28235cb2da498
jiang.wang <jiang.wang@opay-inc.com>
2022-03-29 16:57:33 +0800

    feat: 1
    Signed-off-by: jiang.wang <jiang.wang@opay-inc.com>

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,11 @@
 <template>
   <h3>index</h3>
   <p>{{ state }}</p>
+  <div class="bind">
+    111
+    <p class="bind-title">123</p>
+    <p class="bind-content">321</p>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -8,4 +13,14 @@ import { useGlobalState } from '@/store/index'
 const state = useGlobalState()
 const text = JSON.stringify(state)
 </script>
-<style></style>
+<style lang="scss">
+.bind {
+  color: red;
+  &-title {
+    color: black;
+  }
+  &-content {
+    color: blue;
+  }
+}
+</style>
