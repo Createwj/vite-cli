a8d2918b4999cdeaa54aac92b0bc2b0560b4bb35
jiang.wang <jiang.wang@opay-inc.com>
2022-03-30 17:05:55 +0800

    feat: 1
    Signed-off-by: jiang.wang <jiang.wang@opay-inc.com>

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,7 +8,6 @@ const routes: RouteRecordRaw[] = Object.keys(importRoutes).reduce(
   (routesArr, path) => routesArr.concat(importRoutes[path].default),
   []
 )
-
 function createRouter() {
   return _createRouter({
     history: createWebHistory(),
