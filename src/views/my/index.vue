214b493c41d5aed13fe543eb5bd070af186e3a08
jiang.wang <jiang.wang@opay-inc.com>
2022-04-02 16:29:40 +0800

    feat: 1
    Signed-off-by: jiang.wang <jiang.wang@opay-inc.com>

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="inner-box">
     <h3 class="text-4xl font-bold text-center text-blue-500">index</h3>
-    <h1 class="mt-6 text-2xl font-bold text-gray-900 leading-tight w-48 h-1/2">MIRRORS</h1>
+    <h1 class="mt-6 text-2xl font-bold text-gray-900 leading-tight w-48 h-1/2">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis magni hic laborum, quidem
+      atque ratione temporibus! Quo rerum, esse, quaerat perspiciatis adipisci, dolor accusantium
+      minus quos sunt voluptatem eos quidem.
+    </h1>
     <p class="mt-8 text-2xl text-blue-200">Hello</p>
   </div>
 </template>
